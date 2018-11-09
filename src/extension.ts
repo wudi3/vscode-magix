2f08c0ae00dc98ef75ecd18bda9f13192deb8250
fuyingjun <fuyingjun@alibaba-inc.com>
2018-11-09 10:24:41 +0800

    add:实现简单的ts跳转tmpl 功能

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
+import * as path from 'path';
+const MODE = [{ language: 'html', scheme: 'file' }, { language: 'typescript', scheme: 'file' }];
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -14,16 +15,35 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('mx.plugin', () => {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
+        
         vscode.window.showInformationMessage('Hello World!');
     });
 
     context.subscriptions.push(disposable);
+
+    context.subscriptions.push(vscode.languages.registerDefinitionProvider(MODE, new Definition()));
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+class Definition implements vscode.DefinitionProvider {
+    provideDefinition(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
+        console.info(position);
+        const fileName = document.fileName;
+        const workDir = path.dirname(fileName);
+        const word = document.getText(document.getWordRangeAtPosition(position, new RegExp('\'(.*?)\'|\"(.*?)\"')));
+        const line = document.lineAt(position);
+        //const projectPath = util.getProjectPath(document);
+        let text = line.text.replace(/\s+/g, '');
+        if (text.indexOf('tmpl:') > -1) {
+            let path = workDir + '/' + word.replace(/(^\'*)|(\'*$)/g, '').replace(/(^\"*)|(\"*$)/g, '').replace('@', '');
+            console.log(path);
+            return new vscode.Location(vscode.Uri.file(path), new vscode.Position(0, 0));
+        }
+    }
 }
---
+++ src/initializer.ts
@@ -0,0 +1,7 @@
+import {window,commands} from 'vscode';
+
+export class Initializer{
+   test(params:string) {
+    commands.getCommands(true);
+  }
+}
