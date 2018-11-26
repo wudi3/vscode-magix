51d60e02826a2760b5350d549008dca736218284
fuyingjun <fuyingjun@alibaba-inc.com>
2018-11-26 10:18:49 +0800

    add:添加初始化方法

--- src/initializer.ts
+++ src/initializer.ts
@@ -1,4 +1,4 @@
-import {window,commands} from 'vscode';
+import { window, commands } from 'vscode';
 import { Command } from './command';
 
 import * as fs from 'fs';
@@ -7,50 +7,63 @@ import * as fut from './utils/file-utils';
 import * as parse5 from 'parse5';
 const babylon = require("babylon");
 
-export class Initializer{
-   test(params:string) {
+export class Initializer {
+  test(params: string) {
     commands.getCommands(true);
-    let fileList:Array<string> = [];
-    let rootPath =fut.FileUtils.getProjectPath(undefined);
-    this.listFiles(rootPath,fileList);
+    let fileList: Array<string> = [];
+    let rootPath = fut.FileUtils.getProjectPath(undefined);
+    this.listFiles(rootPath, fileList);
     console.info(fileList);
-    fileList.forEach((filePath)=>{
+    fileList.forEach((filePath) => {
       let extName = path.extname(filePath);
-      if(extName === '.html'){
-      let content = fs.readFileSync(filePath,'UTF-8');
-      let document:parse5.Document =  parse5.parse(content,{scriptingEnabled:false, sourceCodeLocationInfo:true});
-      
-      console.info(document);
+      if (extName === '.html') {
+        let content = fs.readFileSync(filePath, 'UTF-8');
+        let document: parse5.Document = parse5.parse(content, { scriptingEnabled: false, sourceCodeLocationInfo: true });
+
+        console.info(document);
       }
-      else if(extName === '.ts' || extName === '.js'){
+      else if (extName === '.ts' || extName === '.js') {
         console.info(filePath);
-        let content = fs.readFileSync(filePath,'UTF-8');
+        let content = fs.readFileSync(filePath, 'UTF-8');
         try {
-          let doc = babylon.parse(content,{allowImportExportEverywhere:true,allowReturnOutsideFunction:true});
+          let doc = babylon.parse(content, {
+            allowImportExportEverywhere: true, allowReturnOutsideFunction: true, plugins: [
+              // enable jsx and flow syntax
+              "typescript", "estree", "jsx", "flow", "objectRestSpread"
+            ]
+          });
         } catch (error) {
           console.log(error);
         }
-       
-       
+
+
       }
     });
     console.log('done');
   }
-  listFiles(parentPath:string,fileList:Array<string>){
+  listFiles(parentPath: string, fileList: Array<string>) {
     let files = fs.readdirSync(parentPath);
-    
-    files.forEach((item)=>{
-      item = path.join(parentPath,item);
+    if(parentPath.indexOf('/.') > -1 || parentPath.indexOf('node_modules')  > -1 ){
+      return;
+    }
+    console.log(parentPath);
+    files.forEach((item) => {
+      item = path.join(parentPath, item);
       let stat = fs.statSync(item);
-      if(stat.isFile()){
-        fileList.push(item);
-      }else if(stat.isDirectory()){
-        this.listFiles(item,fileList);
+      try {
+        if (stat.isDirectory()) {
+          this.listFiles(item, fileList);
+        }
+        else if (stat.isFile()) {
+          fileList.push(item);
+        }  
+      } catch (error) {
+        console.log(error);
       }
     });
-   
+
   }
-  init(){
+  init() {
     // setInterval(()=>{
     //   Command.MM.push('ttt');
     // },1000);
